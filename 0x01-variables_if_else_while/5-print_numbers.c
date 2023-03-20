a75e02dbfa4f16c2bf5435faf8767c2318e00e90
vessoutraore <yvtraore94@yahoo.com>
2023-03-20 08:45:39 -0700

    Task 5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,9 +3,10 @@
 /**
  * main - Prints all single digit numbers of base 10 starting from 0.
  * 
- * Return: Always 0 if the code is correct.
+ * Return: Always 0.
  */
 int main(void)
+
 {
 	int num;
 
