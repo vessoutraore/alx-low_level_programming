d201a4c8d2517daef8c7dc2137e22fff4fc90334
vessoutraore <yvtraore94@yahoo.com>
2023-03-21 09:58:55 -0700

    Tasks 0. -putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+/** 
+ * Programme that prints _putch , followed by a new line 
+ * 
+ * The program should return 0
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Principal function that print "_putch" followed by a new line .
+ *
+ * Return - Always 0 if the correct is correct.
+ */
+
+int main (void)
+{
+	_putch("_");
+	_putch("p");
+	_putch("u");
+	_putch("t");
+	_putch("c");
+	_putch("h");
+
+	return 0;
+}
